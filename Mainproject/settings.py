2fdce6bbe30452254afebbf160e3ba5a88ad1301
hk <karhat.hamza1@gmail.com>
2021-10-03 12:49:34 -0400

    new fix

--- Mainproject/settings.py
+++ Mainproject/settings.py
@@ -121,6 +121,8 @@ DATABASES = {
 }
 
 
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -149,6 +151,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
